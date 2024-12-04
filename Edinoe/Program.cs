5a300a7633e8b94d11b64ec58a279dfb40a43a7b
Userok <dullahannomer3@gmail.com>
2024-12-04 12:35:36 +0300

    Добавьте файлы проекта.

---
+++ Edinoe/Program.cs
@@ -0,0 +1,32 @@
+﻿class Proga
+{
+    public static void Main() // основная часть
+    {
+        Console.WriteLine("Нужны цыфры: ");
+        int[] n = Array.ConvertAll(Console.ReadLine().Split(new[] { " ", ",", ";" }, StringSplitOptions.RemoveEmptyEntries), int.Parse); //сюда вводят значения игнорируя " ", ",", ";"
+        int[] resultat = UnecalniyNumber(n);
+        Console.WriteLine($"Уникальные числа: {resultat[0]}, {resultat[1]}");
+    }
+    public static int[] UnecalniyNumber(int[] nums) // поиск двух унекальных номеров
+    {
+        int hor = 0;
+        foreach (int x in nums) // поиск основного "или"
+        {
+            hor ^= x;
+        }
+        int diff = hor & -hor; //поиск того что служит разделением
+        int[] res = new int[2];
+        foreach (int x in nums)
+        {
+            if ((x & diff) == 0)
+            {
+                res[0] ^= x;
+            }
+            else
+            {
+                res[1] ^= x;
+            }
+        }
+        return res;
+    }
+}
